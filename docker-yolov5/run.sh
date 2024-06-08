11d5777205d7ad429e3a13048b71e6d792d2c323
Trinadia Kurniasari <109284009+trinadiak@users.noreply.github.com>
2024-06-08 18:56:34 +0700

    Update run.sh

--- docker-yolov5/run.sh
+++ docker-yolov5/run.sh
@@ -10,4 +10,4 @@ xhost +local:
        -e LD_PRELOAD=/usr/lib/aarch64-linux-gnu/libgomp.so.1 \
        -v /tmp/.X11-unix/:/tmp/.X11-unix \
        -v /home/alfatih/docker-yolov5:/weights \
-       yolov5 python3.8 detect_ACM0.py --source 0 --weights /weights/best_new_640.pt
+       yolov5 python3.8 detect_display.py --source 0 --weights /weights/best_new_640.pt
