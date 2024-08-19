a0886c11735463e6f258df00ab1828c855bcfb1f
Trinadia Kurniasari <109284009+trinadiak@users.noreply.github.com>
2024-08-19 22:08:08 +0700

    Update run.sh

--- docker-yolov5/run.sh
+++ docker-yolov5/run.sh
@@ -1,3 +1,16 @@
+#!/bin/bash
+
+# Check if Xvfb is already running
+#if pgrep -x Xvfb >/dev/null; then
+    #echo "Xvfb is already running. Skipping start..."
+#else
+    # Start Xvfb
+    #/usr/bin/Xvfb :1 -screen 0 1024x768x24 &
+    #sleep 2 # Give Xvfb some time to start
+#fi
+
+#export DISPLAY=:1
+# export PYTHONPATH=/weights:$PYTHONPATH
 xhost +local:
 /usr/bin/sudo /usr/bin/docker run \
        -i \
@@ -10,4 +23,4 @@ xhost +local:
        -e LD_PRELOAD=/usr/lib/aarch64-linux-gnu/libgomp.so.1 \
        -v /tmp/.X11-unix/:/tmp/.X11-unix \
        -v /home/alfatih/docker-yolov5:/weights \
-       yolov5 python3.8 detect_display.py --source 0 --weights /weights/best_new_640.pt
+       yolov5 python3.8 detect_final_12.py --source 0 --weights /weights/best_new_640.pt
